6a5ac01d63e5efd5db4c67a4049b5b5e315cf58e
2000guweiqiang <guweiqiang22@163.com>
2019-02-02 19:20:36 +0800

    折半查找

--- 折半查找.py
+++ 折半查找.py
@@ -1,4 +1,16 @@
-import random
-low=[]
-high=[]
-def 
+b=[38,95,1,63,654,46,78,26,11,25,3,66,84,45,8,23,79,100]
+low=b[0]
+high=b[17]
+num=int(input("请输入数字:"))
+if num in b:
+    while True:
+        arr=int((low+high)/2)
+        if num==arr:
+            print("存在此数字")
+            break
+        elif num>arr:
+            low=arr+1
+        elif num<arr:
+            high=arr-1
+else:
+    print("无该数字")
