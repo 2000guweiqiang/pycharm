a8efc4e6b2c50f997ee2f203bb7b64c2e8fd93d2
2000guweiqiang <guweiqiang22@163.com>
2019-02-05 18:30:06 +0800

    线性查找

---
+++ 汉诺塔.py
@@ -0,0 +1,10 @@
+def move(n, a, b, c):
+    if n==1:
+        print (a,'-->',c)
+        return
+    else:
+        move(n-1,a,c,b)  #首先需要把 (N-1) 个圆盘移动到 b
+        move(1,a,b,c)    #将a的最后一个圆盘移动到c
+        move(n-1,b,a,c)  #再将b的(N-1)个圆盘移动到c
+a=int(input("请输入数字:"))
+move(a, 'A', 'B', 'C')
